d2b641aec3170eff04fc49ef1574877bbc6fa3fa
PoelseJonas <jonas.schwarz@hotmail.dk>
2025-09-25 11:06:34 +0200

    Generel opsætning, gettere, settere, variable m.m.

--- src/main/java/domain/AnimalPart.java
+++ src/main/java/domain/AnimalPart.java
@@ -1,4 +1,45 @@
 package domain;
 
 public class AnimalPart{
+
+  private int id;
+  private int weight;
+  private String type;
+
+  public AnimalPart(int id, int weight, String type)
+  {
+    this.id = id;
+    this.weight = weight;
+    this.type = type;
+  }
+
+  public int getWeight()
+  {
+    return weight;
+  }
+
+  public int getId()
+  {
+    return id;
+  }
+
+  public String getType()
+  {
+    return type;
+  }
+
+  public void setWeight(int weight)
+  {
+    this.weight = weight;
+  }
+
+  public void setId(int id)
+  {
+    this.id = id;
+  }
+
+  public void setType(String type)
+  {
+    this.type = type;
+  }
 }
--- src/main/java/domain/Tray.java
+++ src/main/java/domain/Tray.java
@@ -1,4 +1,32 @@
 package domain;
 
 public class Tray{
+
+  private String type;
+  private int weightCapacity;
+
+  public Tray(String type, int weightCapacity) {
+    this.type = type;
+    this.weightCapacity = weightCapacity;
+  }
+
+  public String getType()
+  {
+    return type;
+  }
+
+  public int getWeightCapacity()
+  {
+    return weightCapacity;
+  }
+
+  public void setType(String type)
+  {
+    this.type = type;
+  }
+
+  public void setWeightCapacity(int weightCapacity)
+  {
+    this.weightCapacity = weightCapacity;
+  }
 }

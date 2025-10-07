968b75237b020eb5d118648d58b5c474467bc96b
FPC-VIAUC <354412@via.dk>
2025-10-07 10:24:01 +0200

    Change port on station1 server to 9091

--- src/main/java/slaughterhouse/station1/server/Station1Server.java
+++ src/main/java/slaughterhouse/station1/server/Station1Server.java
@@ -6,7 +6,7 @@ import io.grpc.ServerBuilder;
 public class Station1Server{
   public static void main(String[] args) throws Exception{
     Server server = ServerBuilder
-        .forPort(9090)
+        .forPort(9091)
         .addService(new Station1ServiceImpl())
         .build();
 

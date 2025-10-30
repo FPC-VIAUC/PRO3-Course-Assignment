45093d04476e3d11c9c1e0c640f678feff6abec6
Benjamin K. Hansen <bkh2604@gmail.com>
2025-10-30 11:05:02 +0100

    RequestParam for filtering in arrival dates of the animals

--- src/main/java/slaughterhouse/databaseserver/WebAPI.java
+++ src/main/java/slaughterhouse/databaseserver/WebAPI.java
@@ -4,7 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import slaughterhouse.domain.Animal;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class WebAPI{
@@ -14,5 +20,17 @@ public class WebAPI{
   @GetMapping("/hello")
   public ResponseEntity getHello(){
     return new ResponseEntity (dao.getAnimal(1), HttpStatus.OK);
+  }
+
+  @GetMapping("/animals")
+  public ResponseEntity<List<Animal>> getAnimalsFromDate(@RequestParam("date") String date)
+  {
+    List<Animal> animalsFromDate = new ArrayList<>();
+    for (Animal animal : dao.getAnimals())
+        {
+          if (animal.getDate().toString().equals(date))
+            animalsFromDate.add(animal);
+        }
+    return new ResponseEntity (animalsFromDate, HttpStatus.OK);
   }
 }

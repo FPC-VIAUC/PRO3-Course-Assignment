fc7536146e82832b260af700bfbd7ed54ac56744
FPC-VIAUC <354412@via.dk>
2025-10-30 12:31:30 +0100

    Add WebAPI endpoint to get all animals

--- src/main/java/slaughterhouse/databaseserver/WebAPI.java
+++ src/main/java/slaughterhouse/databaseserver/WebAPI.java
@@ -5,14 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import slaughterhouse.domain.Animal;
+
+import java.util.List;
 
 @RestController
 public class WebAPI{
   @Autowired
   private DatabaseDAO dao;
 
-  @GetMapping("/hello")
-  public ResponseEntity getHello(){
-    return new ResponseEntity (dao.getAnimal(1), HttpStatus.OK);
+  @GetMapping("/animals")
+  public ResponseEntity<List<Animal>> getAnimals(){
+    List<Animal> allAnimals = dao.getAnimals();
+    return new ResponseEntity<List<Animal>>(allAnimals, HttpStatus.OK);
   }
 }

2c31759546c149d30e52e9dcdf3e053b3391522e
PoelseJonas <jonas.schwarz@hotmail.dk>
2025-10-30 11:05:13 +0100

    Kan nu "get" dyr som har en specifik oprindelse. vha. requestparam, som også hedder [FromQuery] i c# :)

--- src/main/java/slaughterhouse/databaseserver/WebAPI.java
+++ src/main/java/slaughterhouse/databaseserver/WebAPI.java
@@ -4,7 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import slaughterhouse.domain.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class WebAPI{
@@ -14,5 +19,20 @@ public class WebAPI{
   @GetMapping("/hello")
   public ResponseEntity getHello(){
     return new ResponseEntity (dao.getAnimal(1), HttpStatus.OK);
+  }
+
+
+  @GetMapping("/animals")
+  public ResponseEntity<List<Animal>> getAnimalsByOrigin(@RequestParam("origin") String origin){
+    List<Animal> animals = new ArrayList<>();
+
+    for (Animal animal : dao.getAnimals()) {
+      if(animal.getOrigin().equalsIgnoreCase(origin)){
+        animals.add(animal);
+      }
+
+    }
+
+    return new ResponseEntity<List<Animal>> (animals, HttpStatus.OK);
   }
 }
